ee6835141033172812b1bbde7680d2a9d318a76d
fogma <fogmatic@gmail.com>
2021-07-08 10:58:36 +0300

    init commit

---
+++ src/test/java/ru/fogma/demobank/BankDemo/BankDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fogma.demobank.BankDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
