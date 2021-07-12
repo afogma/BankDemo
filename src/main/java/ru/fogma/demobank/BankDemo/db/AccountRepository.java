06b835091754a395e44df23cad380688a1f01616
fogma <fogmatic@gmail.com>
2021-07-12 16:30:12 +0300

    init commit

--- src/main/java/ru/fogma/demobank/BankDemo/db/AccountRepository.java
+++ src/main/java/ru/fogma/demobank/BankDemo/db/AccountRepository.java
@@ -1,11 +1,17 @@
 package ru.fogma.demobank.BankDemo.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
 
+    @Override
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    Optional<Account> findById(UUID id);
 }
