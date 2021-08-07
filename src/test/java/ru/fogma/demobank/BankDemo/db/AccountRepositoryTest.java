21100cb3e2f2cf15cfe0ba2dede448d17e70afd9
fogma <fogmatic@gmail.com>
2021-08-07 21:50:49 +0300

    test

--- src/test/java/ru/fogma/demobank/BankDemo/db/AccountRepositoryTest.java
+++ src/test/java/ru/fogma/demobank/BankDemo/db/AccountRepositoryTest.java
@@ -2,7 +2,6 @@ package ru.fogma.demobank.BankDemo.db;
 
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,14 +15,14 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import ru.fogma.demobank.BankDemo.model.TransactionDTO;
 import ru.fogma.demobank.BankDemo.service.TransactionService;
 
-import java.util.UUID;
+import java.math.BigDecimal;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import static java.math.BigDecimal.*;
+import static java.math.BigDecimal.TEN;
+import static java.math.BigDecimal.ZERO;
+import static java.util.concurrent.Executors.newFixedThreadPool;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -54,20 +53,31 @@ public class AccountRepositoryTest {
 
     @Test
     public void should_throw_optimistic_lock_exception() throws InterruptedException {
-        final Account source = accountRepository.save(new Account("source", TEN));
-        final Account target = accountRepository.save(new Account("target", ZERO));
-        TransactionDTO transactionDTO =  new TransactionDTO(source.getId(), target.getId(), ONE);
+        var source = accountRepository.save(new Account("source", TEN));
+        var target = accountRepository.save(new Account("target", ZERO));
+        TransactionDTO transactionDTO = new TransactionDTO(source.getId(), target.getId(), BigDecimal.ONE);
 
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-        assertEquals(0, source.getVersion());
+        ExecutorService executor = newFixedThreadPool(2);
 
-        Executable transfer = () -> transactionService.transfer(transactionDTO);
-
-        for (int i = 0; i < 10; i++) {
-            executor.execute(() -> assertThrows(ObjectOptimisticLockingFailureException.class, transfer));
+        for (int i = 0; i < 2; i++) {
+            executor.execute(() -> runTest(transactionDTO));
         }
+
         executor.shutdown();
         executor.awaitTermination(1, TimeUnit.MINUTES);
+
+        var sourceUpdated = accountRepository.findById(source.getId()).get();
+        var targetUpdated = accountRepository.findById(target.getId()).get();
+        assertEquals(new BigDecimal("9"), sourceUpdated.getBalance().stripTrailingZeros());
+        assertEquals(new BigDecimal("1"), targetUpdated.getBalance().stripTrailingZeros());
+    }
+
+    private void runTest(TransactionDTO transactionDTO) {
+        try {
+            transactionService.transfer(transactionDTO);
+        } catch (ObjectOptimisticLockingFailureException ex) {
+            System.out.println("Caught optimistic lock");
+        }
     }
 
 }
